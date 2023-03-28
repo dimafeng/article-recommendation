f9d170a33f12f94a33d88b3fcf05e6e4752d811a
dimafeng <dimafeng@gmail.com>
2023-03-27 22:11:50 -0400

    Updated controller

--- article_recs/web.py
+++ article_recs/web.py
@@ -8,32 +8,41 @@ class CheckpointRequest(BaseModel):
     event_type: str 
     checkpoint: int
 
+class ContentUpdateRequest(BaseModel):
+    content_id: str
+    data: dict
+
 class Controller:
     def __init__(self, context: Context):
         self.context = context
 
         @context.app.get("/")
-        async def read_root():
+        def read_root():
             return {"Hello": "World"}
 
         @context.app.get("/candidates")
-        async def get_candidates(page: int = 0, limit: int = 10):
+        def get_candidates(page: int = 0, limit: int = 10):
             return self.context.database.get_candidates(page, limit)
 
         @context.app.get("/contents")
-        async def get_contents(content_ids: str):
+        def get_contents(content_ids: str):
             return self.context.database.get_contents(content_ids.split(","))
 
+        @context.app.post("/contents/update")
+        def update_contents(request: ContentUpdateRequest):
+            self.context.database.update_content(request.content_id, request.data)
+            return {"result": "ok"}
+
         @context.app.get("/signals")
-        async def get_signals(page: int = 0, limit: int = 10):
+        def get_signals(page: int = 0, limit: int = 10):
             return self.context.database.get_signals(page, limit)
 
         @context.app.get("/events")
-        async def get_events(consumer_id: str, event_type: str, limit: int = 10):
+        def get_events(consumer_id: str, event_type: str, limit: int = 10):
             return self.context.database.read_latest_events(consumer_id, event_type, limit, False)
 
         @context.app.post("/events/checkpoint")
-        async def checkpoint(checkpoint_request: CheckpointRequest):
+        def checkpoint(checkpoint_request: CheckpointRequest):
             self.context.database.checkpoint(
                 checkpoint_request.consumer_id, 
                 checkpoint_request.event_type, 
