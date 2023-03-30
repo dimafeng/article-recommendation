4309a6e932864a820bb15ff8d15c51a96b080e05
dimafeng <dimafeng@gmail.com>
2023-03-29 21:10:58 -0400

    summarizer

---
+++ modules/content_summarizer/main.py
@@ -0,0 +1,33 @@
+import os
+import time
+from api import API
+from summarizer import Summarizer
+
+def process_events(api: API, summarizer: Summarizer):
+    with api.load_events('content_summarizer', 'content_updated') as events:
+        print(f'Processing {len(events)} events...')
+
+        content_ids = []
+        for e in events:
+            if 'text' in e['fields']:
+                content_ids.append(e['content_id'])
+        
+        for content in api.get_content(content_ids):
+            data = content['data']
+            if not data.get('text', None) is None:
+                print(f'Summarizing {content["id"]}')
+                summary = summarizer.summarize(data.get('text', None))
+                print(f'Saving: {summary}')
+                api.update_content(content["id"], {'summary': summary})
+
+def main():
+    api = API(os.environ.get("API_BASE_URL", ""))
+    summarizer = Summarizer()
+
+    while True:
+        process_events(api, summarizer)
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
---
+++ modules/content_summarizer/summarizer.py
@@ -0,0 +1,28 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+class Summarizer():
+    def __init__(self):
+        model = "facebook/bart-large-cnn"
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model)
+        self.summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+    def __summarize_long_text(self, long_text, max_length=1024):
+        # initialize T5 model and tokenizer
+    
+        # split the long text into smaller segments
+        text_segments = [long_text[i:i+max_length] for i in range(0, len(long_text), max_length)]
+
+        # generate summaries for each segment and concatenate them
+        summary = ''
+        for segment in text_segments:
+            input_ids = self.tokenizer.encode('summarize: ' + segment, return_tensors='pt', max_length=max_length, truncation=True)
+            output = self.model.generate(input_ids)
+            summary += self.tokenizer.decode(output[0], skip_special_tokens=True) + ' '
+
+        return summary
+
+    def summarize(self, text):
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        long_summary = self.__summarize_long_text(text)
+        return summarizer(long_summary, max_length=250, min_length=60, do_sample=False)['summary_text']
